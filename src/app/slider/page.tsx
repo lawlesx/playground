c2c9f342945c85a4a54fdf5380576184e8097f42
Aniruddha Sil <welcometohellv@gmail.com>
2024-04-09 22:01:22 +0530

    style: :lipstick: add Slider meta image

--- src/app/slider/page.tsx
+++ src/app/slider/page.tsx
@@ -1,5 +1,23 @@
 import FramerSlider from '@/components/Slider'
 import NonFramerSlider from '@/components/Slider/NonFramerSlider'
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'Playground - Slider',
+  description: 'An intuitive slider that allows you to adjust the amount of coffee and milk in your cup.',
+  openGraph: {
+    title: 'Playground - Slider',
+    description: 'An intuitive slider that allows you to adjust the amount of coffee and milk in your cup.',
+    images: [
+      {
+        url: 'https://res.cloudinary.com/dk7dt0kk3/image/upload/v1712679946/Playground%20Meta%20Images/Slider_Meta_Image_eywnsq.png',
+        width: 1200,
+        height: 630,
+        alt: 'Slider',
+      },
+    ],
+  },
+}
 
 const Slider = () => {
   return (
