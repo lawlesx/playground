ee0a28c443a9d511e1e4d7dcfbcd58b060cc4b92
Aniruddha Sil <welcometohellv@gmail.com>
2024-04-28 16:23:25 +0530

    Update meta data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,32 @@
+import type { Metadata } from 'next'
 import Link from 'next/link'
+
+export const metadata: Metadata = {
+  title: 'Playground',
+  description: 'Playground create new creations and try new animations and tools',
+  openGraph: {
+    title: 'Playground',
+    description: 'Playground create new creations and try new animations and tools',
+    images: {
+      url: 'https://res.cloudinary.com/dk7dt0kk3/image/upload/v1712679946/Playground%20Meta%20Images/Slider_Meta_Image_eywnsq.png',
+      width: 1200,
+      height: 630,
+      alt: 'Slider',
+    },
+    locale: 'en_US',
+    type: 'website',
+  },
+  twitter: {
+    title: 'Playground',
+    description: 'Playground create new creations and try new animations and tools',
+    images:
+      'https://res.cloudinary.com/dk7dt0kk3/image/upload/v1712679946/Playground%20Meta%20Images/Slider_Meta_Image_eywnsq.png',
+    card: 'summary_large_image',
+    creator: '@lawlesx',
+  },
+  authors: { name: 'Aniruddha Sil', url: 'https://lawlesx.vercel.app/' },
+  keywords: ['Framer Motion', 'React', 'Next.js'],
+}
 
 export default function Home() {
   return (
