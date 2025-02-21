29aa72b7041d64740ea0de90575874ef546ff837
Aniruddha Sil <welcometohellv@gmail.com>
2025-02-21 20:40:53 +0530

    feat: :construction: added plain background for sword

---
+++ src/app/sword/page.tsx
@@ -0,0 +1,15 @@
+import './style.css'
+
+const Sword = () => {
+  return (
+    <main className='grid h-screen w-full place-items-center'>
+      <div className='h-[512px] w-[384px] bg-white p-2'>
+        <div className='background flex h-full w-full flex-col items-center pt-14'>
+          <div className='aspect-square w-1/2 rounded-full bg-[#E81B22] '></div>
+        </div>
+      </div>
+    </main>
+  )
+}
+
+export default Sword
