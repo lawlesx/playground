b456eb76c815214a092c6d2948fc694acf13792b
Aniruddha Sil <welcometohellv@gmail.com>
2025-02-21 17:00:32 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,32 +1,4 @@
 import HighlightButton from '@/components/Button/HighlightButton'
-import type { Metadata } from 'next'
-
-export const metadata: Metadata = {
-  title: 'Playground',
-  description: 'Playground create new creations and try new animations and tools',
-  openGraph: {
-    title: 'Playground',
-    description: 'Playground create new creations and try new animations and tools',
-    images: {
-      url: 'https://res.cloudinary.com/dk7dt0kk3/image/upload/v1712679946/Playground%20Meta%20Images/Slider_Meta_Image_eywnsq.png',
-      width: 1200,
-      height: 630,
-      alt: 'Slider',
-    },
-    locale: 'en_US',
-    type: 'website',
-  },
-  twitter: {
-    title: 'Playground',
-    description: 'Playground create new creations and try new animations and tools',
-    images:
-      'https://res.cloudinary.com/dk7dt0kk3/image/upload/v1712679946/Playground%20Meta%20Images/Slider_Meta_Image_eywnsq.png',
-    card: 'summary_large_image',
-    creator: '@lawlesx',
-  },
-  authors: { name: 'Aniruddha Sil', url: 'https://lawlesx.vercel.app/' },
-  keywords: ['Framer Motion', 'React', 'Next.js'],
-}
 
 export default function Home() {
   return (
