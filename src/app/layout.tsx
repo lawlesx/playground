87ab6d77cc13bdfb9d37522409d35cf177896a91
Aniruddha Sil <welcometohellv@gmail.com>
2025-02-21 16:58:40 +0530

    fix: meta tags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,7 +11,29 @@ const petitFormalScript = Petit_Formal_Script({
 
 export const metadata: Metadata = {
   title: 'Playground',
-  description: `Playground where imagination meets reality.`,
+  description: 'A playground where imagination comes to life—create, animate, and innovate.',
+  openGraph: {
+    title: 'Playground',
+    description: 'A playground where imagination comes to life—create, animate, and innovate.',
+    images: {
+      url: 'https://res.cloudinary.com/dk7dt0kk3/image/upload/v1712679946/Playground%20Meta%20Images/Slider_Meta_Image_eywnsq.png',
+      width: 1200,
+      height: 630,
+      alt: 'Slider',
+    },
+    locale: 'en_US',
+    type: 'website',
+  },
+  twitter: {
+    title: 'Playground',
+    description: 'A playground where imagination comes to life—create, animate, and innovate.',
+    images:
+      'https://res.cloudinary.com/dk7dt0kk3/image/upload/v1712679946/Playground%20Meta%20Images/Slider_Meta_Image_eywnsq.png',
+    card: 'summary_large_image',
+    creator: '@lawlesx',
+  },
+  authors: { name: 'Aniruddha Sil', url: 'https://lawlesx.vercel.app/' },
+  keywords: ['Framer Motion', 'React', 'Next.js'],
 }
 
 export default function RootLayout({
